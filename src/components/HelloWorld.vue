f92d326819e1c1661751480334a2097b603454b6
sblack <450627420@qq.com>
2019-01-04 11:35:49 +0800

    百大项目搭建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box">
+    <div class="box1"></div>
+  </div>
+</template>
+
+<script>
+  
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import "../styles/base.scss";
+  .box{
+      width: 100%;
+      height: rem(88px);
+      background: #ccc;
+  }
+</style>
